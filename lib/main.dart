b0c41eef0ff10338adac95dbd33510af49e7a1ca
Thai Nguyen <thaiomega3@gmail.com>
2019-09-29 21:54:36 -0500

    ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+import 'screens/screen1.dart';
+import 'screens/screen2.dart';
+import 'screens/screen3.dart';
+import 'screens/screen4.dart';
+
+// Styles
+final bottomNavTextStyle = TextStyle(color: Colors.white, fontSize: 14.0);
+final toolbarButtonTextStyle = TextStyle(color: Colors.white, fontSize: 14.0);
+final toolbarTextStyle = TextStyle(color: Colors.white, fontSize: 16.0);
+
+// Themes
+final materialThemeData = ThemeData(
+  primarySwatch: Colors.blue,
+  scaffoldBackgroundColor: Colors.white,
+  accentColor: Colors.blue,
+  appBarTheme: AppBarTheme(color: Colors.blue.shade600),
+  primaryColor: Colors.blue,
+  secondaryHeaderColor: Colors.blue,
+  canvasColor: Colors.red,
+  backgroundColor: Colors.red,
+  textTheme: TextTheme().copyWith(body1: TextTheme().body1)
+);
+
+final cupertinoTheme = ThemeData(
+  primaryColor: Colors.blue,
+  backgroundColor: Colors.blue,
+  scaffoldBackgroundColor: Colors.white
+);
+
 
 void main() => runApp(MyApp());
 
@@ -6,36 +37,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return PlatformApp(
+      home: MyHomePage(title: 'Flutter Platform Aware Widgets'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,18 +53,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _tabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -71,41 +69,46 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: getPage(_tabIndex),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _tabIndex,
+          onTap: (index) {
+            setState(() {
+              _tabIndex = index;
+            });
+          },
+        backgroundColor: Colors.blue,
+          items: [
+        BottomNavigationBarItem(
+          icon:  Icon(Icons.business, color: Colors.grey),
+          title:  Text('Screen 1', style: bottomNavTextStyle),
+          activeIcon:  Icon(Icons.business, color: Colors.white),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        BottomNavigationBarItem(
+          icon:  Icon(Icons.person, color: Colors.grey),
+          title:  Text('Screen 2', style: bottomNavTextStyle),
+          activeIcon:  Icon(Icons.person, color: Colors.white),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.note, color: Colors.grey),
+          title: Text(
+            'Screen 3',
+            style: bottomNavTextStyle,
+          ),
+          activeIcon:  Icon(Icons.note, color: Colors.white),
+        )
+      ]),
     );
+  }
+
+  Widget getPage(int tabIndex) {
+    switch (tabIndex) {
+      case 1:
+        return Screen2();
+      case 2:
+        return Screen3();
+      default:
+        return Screen1();
+    }
   }
 }
